aee240c4e55c56c5f42f12099e704cb70fe75761
Yagmur Akincisoylu <71931384+yagmurakincisoylu@users.noreply.github.com>
2022-08-17 13:31:08 +0200

    first commit

---
+++ react info site/src/components/Main.js
@@ -0,0 +1,16 @@
+function Main() {
+  return (
+    <main>
+      <h2>Fun facts about React</h2>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+}
+
+export default Main;
