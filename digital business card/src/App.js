6362100cfe69db7965a1c6fbf26014b1c1c95f5c
Yagmur Akincisoylu <71931384+yagmurakincisoylu@users.noreply.github.com>
2022-08-17 15:01:18 +0200

    first commit

---
+++ digital business card/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Info from "./components/Info";
+import About from "./components/About";
+import Links from "./components/Links";
+
+function App() {
+  return (
+    <div className="container">
+      <Info />
+      <About />
+      <Links />
+    </div>
+  );
+}
+
+export default App;
