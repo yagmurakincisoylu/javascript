d4413b192d1722cc648abb2b26e83038075866a7
Yagmur Akincisoylu <71931384+yagmurakincisoylu@users.noreply.github.com>
2022-08-18 14:34:58 +0200

    first commit

---
+++ AirBnB clone/src/data.js
@@ -0,0 +1,40 @@
+const data = [
+    {
+        id: 1,
+        title: "Life Lessons with Katie Zaferes",
+        price: 136,
+        coverImg: "katie-zaferes.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "Online",
+        openSpots: 0
+    },
+    {
+        id: 2,
+        title: "Learn Wedding Photography",
+        price: 125,
+        coverImg: "wedding-photography.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27
+    },
+    {
+        id: 3,
+        title: "Group Mountain Biking",
+        price: 50,
+        coverImg: "mountain-bike.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 3
+    }
+]
+
+export default data;
