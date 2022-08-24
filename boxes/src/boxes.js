2b6b826e44da4959e9df7ab6b8eee15b1bc35fea
Yagmur Akincisoylu <71931384+yagmurakincisoylu@users.noreply.github.com>
2022-08-24 15:34:56 +0200

    first commit

---
+++ boxes/src/boxes.js
@@ -0,0 +1,44 @@
+const boxes = [
+  {
+    id: 1,
+    on: true
+  },
+  {
+    id: 2,
+    on: false
+  },
+  {
+    id: 3,
+    on: true
+  },
+  {
+    id: 4,
+    on: true
+  },
+  {
+    id: 5,
+    on: true
+  },
+  {
+    id: 6,
+    on: false
+  },
+  {
+    id: 7,
+    on: false
+  },
+  {
+    id: 8,
+    on: false
+  },
+  {
+    id: 9,
+    on: true
+  },
+  {
+    id: 10,
+    on: false
+  }
+]
+
+export default boxes;
