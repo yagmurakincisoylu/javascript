7cd5354d60f1a13e531d718f02fe1976c7e40ab4
Yagmur Akincisoylu <71931384+yagmurakincisoylu@users.noreply.github.com>
2022-11-28 19:16:32 +0100

    first commit

---
+++ mobile restaurant menu/src/menuData.js
@@ -0,0 +1,25 @@
+const menuData = [
+  {
+    id: 0,
+    img: "images/pizza.png",
+    name: "Pizza",
+    ingredients: ["pepperoni", "mushrom", "mozarella"],
+    price: 14
+  },
+  {
+    id: 1,
+    img: "images/hamburger.png",
+    name: "Hamburger",
+    ingredients: ["beef", "cheese", "lettuce"],
+    price: 12
+  },
+    {
+    id: 2,
+    img: "images/beer.png",
+    name: "Beer",
+    ingredients: ["grain, hops, yeast, water"],
+    price: 12
+  }
+]
+
+export default menuData;
